42aa8d05498d4bd8874d3125bf072caef60ea88f
Lucas Carvalho <programacao04@guidoni.com.br>
2017-01-12 21:56:06 -0200

    Init task resource

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ import express from "express";
 const PORT = 3000;
 const app = express();
 
+app.set('json spaces', 4);
+
 app.get('/', (req, res) => res.json({status: "LukTask API"})); 
+
+app.get('/tasks', (req, res) => {
+  res.json({
+    tasks: [
+      {title: "Learning NodeJS"},
+      {title: "Making apis"}
+    ]
+  });
+});
 
 app.listen(PORT, () => console.log("LukTask API - porta ${PORT}"));
