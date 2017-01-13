c56eea994d1de4b6e7b34eb91d36dccd28abb110
Lucas Carvalho <lucas@valhos.com>
2017-01-12 22:33:37 -0200

    Init Sequelize

---
+++ libs/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  database: "luktask",
+  username: "",
+  password: "",
+  params: {
+    dialect: "sqlite",
+    storage: "luktask.sqlite",
+    define: {
+      underscored: true
+    }
+  }
+};
